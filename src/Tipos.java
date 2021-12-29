afa4684574fdd81ef43c4d58cdbb52038814824b
xcarceco <xcarceco@gmail.com>
2021-12-29 11:00:38 +0100

    EjercicioTema1JavaBasico

---
+++ src/Tipos.java
@@ -0,0 +1,24 @@
+public class Tipos {
+    public static void main(String[] args) {
+        byte variable1;
+        variable1 = 1;
+        short variable2 = 2;
+        int variable3 = 3;
+        long variable4;
+        variable4 = 2345;
+        boolean variable5= true;
+        float variable6 = 13.45;
+        double variable7 = 13.4545345242342d;
+        String variable8;
+        variable8 = "ejercicio clase 1 Java";
+
+        System.out.println(variable1);
+        System.out.println(variable2);
+        System.out.println(variable3);
+        System.out.println(variable4);
+        System.out.println(variable5);
+        System.out.println(variable6);
+        System.out.println(variable7);
+        System.out.println(variable8);
+    }
+}
